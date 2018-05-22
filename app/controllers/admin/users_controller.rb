e8714b42d2718c83da828ad2137ec0963741f166
raitenn <rakurusurann@gmail.com>
2018-05-22 11:41:23 +0000

    adminのuserコントローラーの変更

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < ApplicationController
   PER = 10
+  #下のページ数を表している
   def index
     @users = User.all
     @users = User.page(params[:page]).per(PER)
@@ -35,9 +36,11 @@ class Admin::UsersController < ApplicationController
       else
         render :edit
       end
-    else redirect_to admin_users_path
+    else 
+      redirect_to admin_users_path
     end
   end
+
 
   def history
 
@@ -57,6 +60,19 @@ class Admin::UsersController < ApplicationController
   end
 
   def destroy
-    
+    if user = User.find_by(params[:id])
+      user.delete_flag = 2
+      user.save
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to admin_user_path
+    end
   end
+
+  private
+
+   def user_params
+        params.require(:user).permit(:name_kanji, :name_kana, :street_address, :phone_number ,:email)
+        
+    end
+
 end
