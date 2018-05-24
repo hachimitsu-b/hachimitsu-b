c35a2a6b309d9ac6b5f7ceced4fe5508a24f5e46
Masaki Ikegami <salvage0707@gmail.com>
2018-05-24 19:36:23 +0900

    途中

--- app/controllers/admin/recommends_controller.rb
+++ app/controllers/admin/recommends_controller.rb
@@ -1,8 +1,23 @@
-class Admins::RecommendsController < ApplicationController
+class Admin::RecommendsController < ApplicationController
   def index
-  	@type_new = TypeName.new
   	@type_names = TypeName.all
   	@recommend = Recommend.new
-  	@cds = Cd.last(5)
+  	@cds = Cd.last(10)
   end
+
+  def create
+  	@recommend = Recommend.new(recommend_params)
+  	if @recommend.save
+  		redirect_to admin_recommend_path
+  	else
+  		@type_names = TypeName.all
+  		render 'index'
+  	end
+  end
+
+  private
+
+  	def recommend_params
+  		params.require(:recommend).permit(:recomend_type, :cd_id)
+  	end
 end
