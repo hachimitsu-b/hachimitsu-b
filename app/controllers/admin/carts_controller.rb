b1819377cf5e06563149b194352583d39f8f14f8
raitenn <rakurusurann@gmail.com>
2018-05-28 05:20:48 +0000

    検索機能実装中

--- app/controllers/admin/carts_controller.rb
+++ app/controllers/admin/carts_controller.rb
@@ -1,18 +1,18 @@
 class Admin::CartsController < ApplicationController
   def index
-
-  	# @carts = Cart.where(status_flag: 1)
-  	@carts = Cart.all
-  	a = User.where(name_kanji: params[:search])
+  	# if params[:search]
+	  	@carts = Cart.where(status_flag: 1)
+	  	# binding.pry
+	  	@a = User.where('name_kanji LIKE(?)', "%#{params[:search]}%")
+	 # else
+	 # 	@carts = Cart.where(status_flag: 1)
+	 # end
   end
 
   def send_cart
   	@cart = Cart.find(params[:id])
-    if @cart.update(buy_cds_update_params)
-      @user.carts.create(status_flag: 0)
-      @cart.status_flag = 2
-      @cart.save
-    
-    end
+    @cart.status_flag = 2
+    @cart.save
+    redirect_to admin_carts_path
   end
 end
